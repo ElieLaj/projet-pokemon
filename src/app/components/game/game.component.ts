4967664aaf89b4dfb843c3109d17b57e1708baf2
victor haettinger <elie.lajoinie@supdevinci-edu.fr>
2024-11-20 17:06:03 +0100

    Fix import

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { Monster } from '../../models/monster.model';
 import { BattleScreenComponent } from '../battle-screen/battle-screen.component';
-import { Game } from '../../models/game.model';
 import { Attack } from '../../models/attack.model';
 import { MonsterComponent } from '../monster/monster.component';
+
 @Component({
   selector: 'app-game',
   standalone: true,
@@ -11,6 +11,7 @@ import { MonsterComponent } from '../monster/monster.component';
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
+
 export class GameComponent {
   scratch = new Attack('Scratch', 10, 'Normal');
   playerMonster = new Monster(1, 'Bulbasaur', 'Grass', 100, 25, 15, [this.scratch]);
