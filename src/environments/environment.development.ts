809ca870b057d907eb21bc3bcc884f0ca033a402
victor haettinger <elie.lajoinie@supdevinci-edu.fr>
2024-11-28 10:42:31 +0100

    Update des clés

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiURL: 'https://pokemon-api-spring.onrender.com/api/v1/',
+  apiURL: 'https://pokemon-api-spring.onrender.com/api/v1',
   apiKey: 'wTdm2HVWcPBCpT5'
 };
