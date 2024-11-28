22c253eabd5ecdcc4056a1676e1cfe4b06bc6add
victor haettinger <elie.lajoinie@supdevinci-edu.fr>
2024-11-28 23:45:21 +0100

    route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,10 @@
 import { Routes } from '@angular/router';
 import { GameComponent } from './components/game/game.component';
-import { AddDataComponent } from './components/add-data/add-data.component';
 
 export const routes: Routes = [
     {
         path: '',
         component: GameComponent
-    },
-    {
-        path: 'admin',
-        component: AddDataComponent
     },
     {
         path: '**',
